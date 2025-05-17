a7a6d10579f4696fa1f352bfefcd82a39571e304
yiming-liao <yimingliao.official@gmail.com>
2025-05-17 22:28:26 +0700

    Remove unused mock data

--- __test__/mock/mock-fallback-locales.ts
+++
@@ -1,8 +0,0 @@
-import type { FallbackLocalesMap } from "../../src/intor/types/message-structure-types";
-
-export const mockFallbackLocales: FallbackLocalesMap = {
-  "en-US": ["fr-FR", "zh-TW"],
-  "fr-FR": ["en-US"],
-  "zh-TW": ["en-US"],
-  "ja-JP": [],
-};
--- __test__/mock/mock-messages.ts
+++
@@ -1,4 +0,0 @@
-import type { Message } from "../../src/intor/types/message-structure-types";
-
-export const mockMessage: Message =
-  "Hello, {user.profile.name}. You have {user.notifications.count} new messages. Your last login was {user.activity.lastLogin.date} at {user.activity.lastLogin.time}.";
